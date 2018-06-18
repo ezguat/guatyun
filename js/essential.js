49bc9fe57d0ae7f823f15a77f0c89969a4af7ba5
ezguat <467006015@qq.com>
2018-06-18 14:48:08 +0800

    debug

--- js/essential.js
+++ js/essential.js
@@ -69,6 +69,88 @@ if($.cookie("username")){
 }
 design_mobile_header();
 function design_mobile_header() {
+    // 生存隐藏的菜单
+    var div1=document.createElement("div");
+    div1.setAttribute("id","canvas");
+    div1.setAttribute("style","position: absolute;height: 94%;margin-top: 0%;width: 100%;background-color: black;opacity: 0.9;z-index: 999;display: none");
+    $("#mobile").prepend(div1);// 追加新元素
+    var div1=document.createElement("div");
+    div1.setAttribute("id","canvas-div1");
+    div1.setAttribute("style","border-bottom: 1px silver solid;margin-top: 1%;height: 8%;width: 100%");
+    $("#canvas").prepend(div1);// 追加新元素
+    var div1=document.createElement("a");
+    div1.setAttribute("id","canvas-p1");
+    div1.setAttribute("style","text-decoration: none;color: white");
+    div1.setAttribute("href","../index.html");
+    $("#canvas-div1").prepend(div1);// 追加新元素
+    var div1=document.createElement("p");
+    div1.setAttribute("id","canvas-a1");
+    div1.setAttribute("style","font-family: 'Microsoft YaHei UI';font-size: large;margin-left: 5%");
+    $("#canvas-p1").prepend(div1);// 追加新元素
+    document.getElementById('canvas-a1').innerHTML="首页";
+    var div1=document.createElement("div");
+    div1.setAttribute("id","canvas-div2");
+    div1.setAttribute("style","border-bottom: 1px silver solid;margin-top: 1%;height: 8%;width: 100%");
+    $("#canvas").append(div1);// 追加新元素
+    var div1=document.createElement("a");
+    div1.setAttribute("id","canvas-p2");
+    div1.setAttribute("style","text-decoration: none;color: white");
+    div1.setAttribute("href","./index.html");
+    $("#canvas-div2").prepend(div1);// 追加新元素
+    var div1=document.createElement("p");
+    div1.setAttribute("id","canvas-a2");
+    div1.setAttribute("style","font-family: 'Microsoft YaHei UI';font-size: large;margin-left: 5%");
+    $("#canvas-p2").prepend(div1);// 追加新元素
+    document.getElementById('canvas-a2').innerHTML="帮助中心";
+    var div1=document.createElement("div");
+    div1.setAttribute("id","canvas-div3");
+    div1.setAttribute("style","border-bottom: 1px silver solid;margin-top: 1%;height: 8%;width: 100%");
+    $("#canvas").append(div1);// 追加新元素
+    var div1=document.createElement("a");
+    div1.setAttribute("id","canvas-p3");
+    div1.setAttribute("style","text-decoration: none;color: white");
+    div1.setAttribute("href","./index.html");
+    $("#canvas-div3").prepend(div1);// 追加新元素
+    var div1=document.createElement("p");
+    div1.setAttribute("id","canvas-a3");
+    div1.setAttribute("style","font-family: 'Microsoft YaHei UI';font-size: large;margin-left: 5%");
+    $("#canvas-p3").prepend(div1);// 追加新元素
+    document.getElementById('canvas-a3').innerHTML="留言反馈";
+    var div1=document.createElement("div");
+    div1.setAttribute("id","canvas-div4");
+    div1.setAttribute("style","border-bottom: 1px silver solid;margin-top: 1%;height: 8%;width: 100%");
+    $("#canvas").append(div1);// 追加新元素
+    var div1=document.createElement("a");
+    div1.setAttribute("id","canvas-p4");
+    div1.setAttribute("style","text-decoration: none;color: white");
+    div1.setAttribute("href","./index.html");
+    $("#canvas-div4").prepend(div1);// 追加新元素
+    var div1=document.createElement("p");
+    div1.setAttribute("id","canvas-a4");
+    div1.setAttribute("style","font-family: 'Microsoft YaHei UI';font-size: large;margin-left: 5%");
+    $("#canvas-p4").prepend(div1);// 追加新元素
+    document.getElementById('canvas-a4').innerHTML="加入我们";
+    var div1=document.createElement("div");
+    div1.setAttribute("id","canvas-div5");
+    div1.setAttribute("style","border-bottom: 1px silver solid;margin-top: 1%;height: 8%;width: 100%");
+    $("#canvas").append(div1);// 追加新元素
+    var div1=document.createElement("a");
+    div1.setAttribute("id","canvas-p5");
+    div1.setAttribute("style","text-decoration: none;color: white");
+    div1.setAttribute("href","./index.html");
+    $("#canvas-div5").prepend(div1);// 追加新元素
+    var div1=document.createElement("p");
+    div1.setAttribute("id","canvas-a5");
+    div1.setAttribute("style","font-family: 'Microsoft YaHei UI';font-size: large;margin-left: 5%");
+    $("#canvas-p5").prepend(div1);// 追加新元素
+    document.getElementById('canvas-a5').innerHTML="关于我们";
+    var div1=document.createElement("p");
+    div1.setAttribute("id","canvas-p7");
+    div1.setAttribute("style","margin-left: 80%;font-family: 'Microsoft YaHei UI';font-size: 20px;color: whitesmoke;margin-top: 40%");
+    div1.setAttribute("onclick","$('#canvas').css('display','none')");
+    $("#canvas").append(div1);// 追加新元素
+    document.getElementById('canvas-p7').innerHTML="收起";
+    // 生存顶部菜单栏
     var div=document.createElement("div");
     div.setAttribute("id","header");
     div.setAttribute("style","width: 100%;height:8%;background-color: #00c1de;");
@@ -80,8 +162,6 @@ function design_mobile_header() {
     var a=document.createElement("a");
     a.setAttribute("id","link1-m");
     a.setAttribute("style","text-decoration: none;color: white");
-    a.setAttribute("onmouseenter","this.style='text-decoration: none;color:black'");
-    a.setAttribute("onmouseleave","this.style='text-decoration: none;color:white'");
     a.setAttribute("href","../content/login.html");
     $("#name-m").append(a);// 追加新元素
     var p=document.createElement("p");
@@ -89,7 +169,6 @@ function design_mobile_header() {
     p.setAttribute("style","font-family: 'Microsoft YaHei UI';font-size: medium");
     $("#link1-m").append(p);// 追加新元素
     document.getElementById('change-m').innerHTML="登录";
-
     var div1=document.createElement("div");
     div1.setAttribute("id","gone-m");
     div1.setAttribute("style","margin-left: 0%;height: 100%;width: 15%;display: inline-block");
@@ -97,8 +176,6 @@ function design_mobile_header() {
     var a=document.createElement("a");
     a.setAttribute("id","link2-m");
     a.setAttribute("style","text-decoration: none;color: white");
-    a.setAttribute("onmouseenter","this.style='text-decoration: none;color:black'");
-    a.setAttribute("onmouseleave","this.style='text-decoration: none;color:white'");
     a.setAttribute("href","../content/register.html");
     $("#gone-m").append(a);// 追加新元素
     var p=document.createElement("p");
@@ -113,8 +190,6 @@ function design_mobile_header() {
     var a=document.createElement("a");
     a.setAttribute("id","link3-m");
     a.setAttribute("style","text-decoration: none;color: white");
-    a.setAttribute("onmouseenter","this.style='text-decoration: none;color:black'");
-    a.setAttribute("onmouseleave","this.style='text-decoration: none;color:white'");
     a.setAttribute("href","#");
     $("#show-m").append(a);// 追加新元素
     var p=document.createElement("p");
@@ -124,7 +199,7 @@ function design_mobile_header() {
     document.getElementById('change9-m').innerHTML="注销";
     var div1=document.createElement("span");
     div1.setAttribute("id","go-m");
-    div1.setAttribute("style","color: white");
+    div1.setAttribute("style","color: white;height:30px;width:40px");
     div1.setAttribute("class","glyphicon glyphicon-menu-hamburger");
     div1.setAttribute("onclick"," $('#canvas').css('display','block')");
     $("#header").append(div1);// 追加新元素
